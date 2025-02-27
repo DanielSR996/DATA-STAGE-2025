bcbd83262e1d07b6314c00f3fcf70e672687e68b
DanielSR996 <danielsilvaramirez.dsr@gmail.com>
2025-02-27 15:05:36 -0600

    Cambio Vista

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -4,6 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -13,8 +14,9 @@ const Navbar = () => {
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             Subir ZIP
           </Typography>
-          <Button color="inherit">Subir ZIP</Button>
-          <Button color="inherit">Subir ASC</Button>
+          <Button color="inherit" component={Link} to="/upload">Subir ZIP</Button>
+          <Button color="inherit" component={Link} to="/upload-asc">Subir ASC</Button>
+          <Button color="inherit" component={Link} to="/tablas">Tablas</Button>
         </Toolbar>
       </Container>
     </AppBar>
