d4fe8516d7a308b026df4cda23530732ca322da8
DanielSR996 <danielsilvaramirez.dsr@gmail.com>
2025-03-26 11:23:00 -0600

    nuevo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['8d53-177-241-248-42.ngrok-free.app'], // Agrega tu host aquí
-    host: true, // Permite acceder desde cualquier IP
-    port: 5173 // Asegúrate de usar el puerto correcto
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://8d53-177-241-248-42.ngrok-free.app',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
 
